501a59ae0c3603d7c4503d4535de86f2b0ca3fd9
Yongjun Park <yopark@student.42seoul.kr>
2024-04-07 21:32:47 +0900

    style(prettier): execute pnpm format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import QueryProvider from '@/providers/query-provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import QueryProvider from '@/providers/query-provider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { type PropsWithChildren, useState } from 'react';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren, useState } from 'react';
 
 const QueryProvider = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
