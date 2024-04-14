89b68be2ba7061cf393c1bf6a08af287e9d0e829
Yongjun Park <yopark@student.42seoul.kr>
2024-04-14 21:21:58 +0900

    feat(ui): customize ui component - RadioGroup

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -29,7 +29,10 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'aspect-square h-4 w-4 rounded-full transition',
+        'border border-blccu-neutral-400 text-blccu-neutral-800',
+        'focus:outline-none focus-visible:border-blccu-neutral-800 focus-visible:ring-2 focus-visible:ring-blccu-ring',
+        'disabled:cursor-not-allowed disabled:bg-blccu-neutral-200 disabled:opacity-50',
         className,
       )}
       {...props}
