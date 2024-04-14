8cfd5f91f692f1856879742952b57161ad0f32c5
Yongjun Park <yopark@student.42seoul.kr>
2024-04-14 21:20:43 +0900

    feat(ui): customize ui component - input

--- components/ui/input.stories.tsx
+++ components/ui/input.stories.tsx
@@ -1,15 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const meta: Meta<typeof Input> = {
   title: 'Components/Input',
   component: Input,
   argTypes: {
-    type: {
-      options: ['text'],
-      control: { type: 'radio' },
-    },
     disabled: {
       control: { type: 'boolean' },
     },
@@ -23,12 +20,23 @@ type Story = StoryObj<typeof Input>;
 
 const Playground: Story = {
   args: {
-    type: 'text',
-    disabled: false,
-    placeholder: 'Write something...',
+    placeholder: '이름을 입력해주세요.',
   },
 };
 
-export { Playground };
+const WithLabel: Story = {
+  args: {
+    id: 'username',
+    placeholder: '이름을 입력해주세요.',
+  },
+  render: (args) => (
+    <div className="flex flex-col gap-4">
+      <Label htmlFor={args.id}>유저 이름</Label>
+      <Input {...args} />
+    </div>
+  ),
+};
+
+export { Playground, WithLabel };
 
 export default meta;
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex w-full rounded-md px-4 py-3 text-sm transition',
+          'border border-blccu-input',
+          'file:border-0 file:bg-transparent file:text-sm file:font-medium',
+          'placeholder:text-blccu-neutral-600',
+          'focus-visible:border-blccu-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blccu-ring',
+          'disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
