d1ecc9cb78d0345ffd156b520c898ed3626c1e1e
Yongjun Park <yopark@student.42seoul.kr>
2024-04-16 21:37:37 +0900

    feat(ui): create ui component - HorizontalScrollablePostCard

---
+++ components/ui-unstable/horizontal-scrollable-post-card.tsx
@@ -0,0 +1,43 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { cn } from '@/lib/utils';
+
+type HorizontalScrollablePostCardProps = {
+  username: string;
+  avatar: string;
+  title: string;
+  thumbnail: string;
+};
+
+const HorizontalScrollablePostCard = ({
+  username,
+  avatar,
+  title,
+  thumbnail,
+}: HorizontalScrollablePostCardProps) => {
+  return (
+    <figure
+      className={cn(
+        'relative flex h-52 w-36 flex-col rounded-lg bg-cover bg-center shadow-lg transition',
+        'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blccu-ring',
+      )}
+      style={{
+        backgroundImage: `url(${thumbnail})`,
+      }}
+    >
+      <div className="absolute bottom-0 flex w-full items-center gap-2 rounded-b-lg bg-blccu-white px-2 py-3">
+        <Avatar size="xs">
+          <AvatarImage src={avatar} alt="avatar" />
+          <AvatarFallback className="bg-blccu-neutral-400" />
+        </Avatar>
+        <div className="flex w-full flex-col">
+          <h3 className="line-clamp-1 text-sm">{title}</h3>
+          <p className="line-clamp-1 text-2xs font-light text-blccu-neutral-600">
+            {username}
+          </p>
+        </div>
+      </div>
+    </figure>
+  );
+};
+
+export { HorizontalScrollablePostCard };
