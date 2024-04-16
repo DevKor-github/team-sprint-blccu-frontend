20f2f24b02bbf4b7e66f2c89f8e1f2e512b31479
Yongjun Park <yopark@student.42seoul.kr>
2024-04-16 21:38:08 +0900

    feat(ui): create ui component - StackedUserCard

---
+++ components/ui-unstable/stacked-user-card.stories.tsx
@@ -0,0 +1,118 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { StackedUserCard } from '@/components/ui-unstable/stacked-user-card';
+import { Button } from '@/components/ui/button';
+
+const meta: Meta<typeof StackedUserCard> = {
+  title: 'Unstable/StackedUserCard',
+  component: StackedUserCard,
+  argTypes: {
+    avatar: {
+      control: {
+        type: 'text',
+      },
+    },
+    username: {
+      control: {
+        type: 'text',
+      },
+    },
+    description: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: {
+      exclude: ['right'],
+    },
+  },
+};
+
+type Story = StoryObj<typeof StackedUserCard>;
+
+const Playground: Story = {
+  args: {
+    avatar:
+      'https://preview.free3d.com/img/2018/04/2269230414092568255/4t4ll0dg.jpg',
+    username: '이오리',
+    description:
+      '인테리어나 집꾸미기에 관심이 많아요! 여러분의 집도 함께 꾸며드릴까요?',
+    right: <Button size="sm">팔로우</Button>,
+  },
+};
+
+const exampleData = [
+  {
+    avatar:
+      'https://preview.free3d.com/img/2018/04/2269230414092568255/4t4ll0dg.jpg',
+    username: '이오리',
+    description:
+      '인테리어나 집꾸미기에 관심이 많아요! 여러분의 집도 함께 꾸며드릴까요?',
+    right: <Button size="sm">팔로우</Button>,
+  },
+  {
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Loutre_g%C3%A9ante_gros_plan.jpg/1200px-Loutre_g%C3%A9ante_gros_plan.jpg',
+    username: '김수달',
+    description: '여러분과 함께 즐거운 시간을 보내고 싶어요.',
+    right: (
+      <Button size="sm" variant="secondary">
+        팔로잉
+      </Button>
+    ),
+  },
+  {
+    avatar:
+      'https://images.chosun.com/resizer/ebyov8zMQwFBKMq6NLp3W0MAtCs=/500x475/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/NRVFQKTQC5DCXNDXPJMTONFHJA.jpg',
+    username: '양가람',
+    description: '여행을 좋아하는 양가람입니다.',
+  },
+  {
+    avatar:
+      'https://preview.free3d.com/img/2018/04/2269230414092568255/4t4ll0dg.jpg',
+    username: '이오리',
+    description:
+      '인테리어나 집꾸미기에 관심이 많아요! 여러분의 집도 함께 꾸며드릴까요?',
+    right: <Button size="sm">팔로우</Button>,
+  },
+  {
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Loutre_g%C3%A9ante_gros_plan.jpg/1200px-Loutre_g%C3%A9ante_gros_plan.jpg',
+    username: '김수달',
+    description: '여러분과 함께 즐거운 시간을 보내고 싶어요.',
+    right: (
+      <Button size="sm" variant="secondary">
+        팔로잉
+      </Button>
+    ),
+  },
+  {
+    avatar:
+      'https://images.chosun.com/resizer/ebyov8zMQwFBKMq6NLp3W0MAtCs=/500x475/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/NRVFQKTQC5DCXNDXPJMTONFHJA.jpg',
+    username: '양가람',
+    description: '여행을 좋아하는 양가람입니다.',
+    right: <Button size="sm">팔로우</Button>,
+  },
+  {
+    avatar:
+      'https://preview.free3d.com/img/2018/04/2269230414092568255/4t4ll0dg.jpg',
+    username: '이오리',
+    description:
+      '인테리어나 집꾸미기에 관심이 많아요! 여러분의 집도 함께 꾸며드릴까요?',
+  },
+] as const;
+
+const Example: Story = {
+  render: () => (
+    <div className="flex flex-col gap-2">
+      {exampleData.map((data, index) => (
+        <StackedUserCard key={index} {...data} />
+      ))}
+    </div>
+  ),
+};
+
+export { Example, Playground };
+export default meta;
