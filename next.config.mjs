b4d1cdd84fe614721f84036a0a47ef8b9080a913
Yongjun Park <yopark@student.42seoul.kr>
2024-04-17 17:02:18 +0900

    chore(next.config.js): add config for webpack to use @svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -44,6 +44,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
