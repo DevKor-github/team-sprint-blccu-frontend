3a0c46070c055a0b42bb9d9e44efcff3601c7144
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 04:46:57 +0900

    feat(public-layout): scaffold - FollowersPage, FollowingPage

--- app/(public)/users/[username]/followers/page.tsx
+++ app/(public)/users/[username]/followers/page.tsx
@@ -1,11 +1,60 @@
+import {
+  AppBar,
+  AppBarBack,
+  AppBarTitle,
+} from '@/components/ui-unstable/app-bar';
+import { StackedUserCard } from '@/components/ui-unstable/stacked-user-card';
+import { Button } from '@/components/ui/button';
+import { generateUsers, sampleSize } from '@/lib/utils';
+
+const USERS_COUNT = 21;
+
+const users = generateUsers(USERS_COUNT);
+
+const FollowingButtons = [
+  <Button key="follow-button" size="sm">
+    팔로우
+  </Button>,
+  <Button key="following-button" variant="secondary" size="sm">
+    팔로잉
+  </Button>,
+];
+
+const FollowingButton = sampleSize(FollowingButtons, USERS_COUNT);
+
 type FollowersPageProps = {
   params: {
     username: string;
   };
 };
 
-const FollowersPage = ({ params: { username } }: FollowersPageProps) => {
-  return <div>FollowersPage (username: {username})</div>;
+const FollowersPage = ({ params: { username: _ } }: FollowersPageProps) => {
+  return (
+    <div>
+      <AppBar>
+        <AppBarBack />
+        <AppBarTitle>팔로워 목록</AppBarTitle>
+      </AppBar>
+      <div className="pt-14">
+        <div className="mt-4 flex flex-col gap-2 px-4">
+          {users.map(
+            (
+              { username, handle, description, profileImage, backgroundImage },
+              index,
+            ) => (
+              <StackedUserCard
+                key={index}
+                avatar={profileImage}
+                username={username}
+                description={description}
+                right={FollowingButton[index]}
+              />
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FollowersPage;
--- app/(public)/users/[username]/following/page.tsx
+++ app/(public)/users/[username]/following/page.tsx
@@ -1,11 +1,58 @@
+import {
+  AppBar,
+  AppBarBack,
+  AppBarTitle,
+} from '@/components/ui-unstable/app-bar';
+import { StackedUserCard } from '@/components/ui-unstable/stacked-user-card';
+import { Button } from '@/components/ui/button';
+import { generateUsers, sampleSize } from '@/lib/utils';
+
+const users = generateUsers(21);
+
+const FollowingButtons = [
+  <Button key="follow-button" size="sm">
+    팔로우
+  </Button>,
+  <Button key="following-button" variant="secondary" size="sm">
+    팔로잉
+  </Button>,
+];
+
+const FollowingButton = sampleSize(FollowingButtons, 21);
+
 type FollowingPageProps = {
   params: {
     username: string;
   };
 };
 
-const FollowingPage = ({ params: { username } }: FollowingPageProps) => {
-  return <div>FollowingPage (username: {username})</div>;
+const FollowingPage = ({ params: { username: _ } }: FollowingPageProps) => {
+  return (
+    <div>
+      <AppBar>
+        <AppBarBack />
+        <AppBarTitle>팔로잉 목록</AppBarTitle>
+      </AppBar>
+      <div className="pt-14">
+        <div className="mt-4 flex flex-col gap-2 px-4">
+          {users.map(
+            (
+              { username, handle, description, profileImage, backgroundImage },
+              index,
+            ) => (
+              <StackedUserCard
+                key={index}
+                avatar={profileImage}
+                username={username}
+                description={description}
+                right={FollowingButton[index]}
+              />
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FollowingPage;
