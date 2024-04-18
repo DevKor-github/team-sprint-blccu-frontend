12c4a5974c4b4c1e572863edfa6b56d7f4c71249
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 04:39:07 +0900

    feat(public-layout): scaffold - MainLayout

--- app/(public)/(main)/layout.tsx
+++ app/(public)/(main)/layout.tsx
@@ -1,7 +1,14 @@
 import { type PropsWithChildren } from 'react';
 
+import { MainLayoutBottomNavBar } from '@/features/main-layout/main-layout-bottom-nav-bar';
+
 const MainLayout = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+  return (
+    <main>
+      <div className="pb-24">{children}</div>
+      <MainLayoutBottomNavBar />
+    </main>
+  );
 };
 
 export default MainLayout;
---
+++ features/main-layout/main-layout-bottom-nav-bar.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+
+import { Home, Pencil, User } from 'lucide-react';
+
+import { ROUTES } from '@/constants/routes';
+import { cn, generateUser } from '@/lib/utils';
+
+const me = generateUser();
+
+const { username } = me;
+
+const MainLayoutBottomNavBar = () => {
+  return (
+    <nav
+      className={cn(
+        'fixed bottom-0 left-1/2 w-full max-w-md -translate-x-1/2',
+        'border-t bg-blccu-white',
+      )}
+    >
+      <ul className="flex justify-evenly">
+        <li>
+          <Link href={ROUTES.ROOT}>
+            <div className="p-5">
+              <Home className="h-5 w-5" />
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link href={ROUTES.WRITE}>
+            <div
+              className={cn(
+                'fixed bottom-[30px] left-1/2 -translate-x-1/2',
+                'rounded-full bg-blccu-black p-4 text-white',
+              )}
+            >
+              <Pencil className="h-6 w-6" />
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link href={ROUTES.USERNAME_OF(username)}>
+            <div className="p-5">
+              <User className="h-5 w-5" />
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { MainLayoutBottomNavBar };
