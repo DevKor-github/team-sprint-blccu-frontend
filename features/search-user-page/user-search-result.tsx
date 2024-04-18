d59dfaf4000007c1ee4353e3a9113a7274d29165
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 04:44:06 +0900

    feat(public-layout): scaffold - SearchUserPage

---
+++ features/search-user-page/user-search-result.tsx
@@ -0,0 +1,41 @@
+import { StackedUserCard } from '@/components/ui-unstable/stacked-user-card';
+import { Button } from '@/components/ui/button';
+import { generateUsers, sampleSize } from '@/lib/utils';
+
+const USERS_COUNT = 21;
+
+const users = generateUsers(USERS_COUNT);
+
+const FollowingButtons = [
+  <Button key="follow-button" size="sm">
+    팔로우
+  </Button>,
+  <Button key="following-button" variant="secondary" size="sm">
+    팔로잉
+  </Button>,
+];
+
+const FollowingButton = sampleSize(FollowingButtons, USERS_COUNT);
+
+const UserSearchResult = () => {
+  return (
+    <div className="mt-4 flex flex-col gap-2 px-4">
+      {users.map(
+        (
+          { username, handle, description, profileImage, backgroundImage },
+          index,
+        ) => (
+          <StackedUserCard
+            key={index}
+            avatar={profileImage}
+            username={username}
+            description={description}
+            right={FollowingButton[index]}
+          />
+        ),
+      )}
+    </div>
+  );
+};
+
+export { UserSearchResult };
