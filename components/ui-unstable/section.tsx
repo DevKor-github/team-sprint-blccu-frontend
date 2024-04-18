5177aedb6b861a942e9a410841b38b4b5b77de5e
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 04:35:48 +0900

    feat: create Section

---
+++ components/ui-unstable/section.tsx
@@ -0,0 +1,29 @@
+import { type PropsWithChildren } from 'react';
+
+import { cn } from '@/lib/utils';
+import { type PropsWithClassName } from '@/types/props';
+
+const Section = ({
+  children,
+  className,
+}: PropsWithChildren & PropsWithClassName) => {
+  return <section className={cn('mt-8', className)}>{children}</section>;
+};
+
+const SectionTitle = ({
+  children,
+  className,
+}: PropsWithChildren & PropsWithClassName) => {
+  return (
+    <h2 className={cn('ml-1 text-lg font-bold', className)}>{children}</h2>
+  );
+};
+
+const SectionContent = ({
+  children,
+  className,
+}: PropsWithChildren & PropsWithClassName) => {
+  return <div className={cn('mt-4', className)}>{children}</div>;
+};
+
+export { Section, SectionContent, SectionTitle };
