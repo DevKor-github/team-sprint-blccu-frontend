203f5ad8057ebefe03164e4a161060099193aaea
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 04:51:20 +0900

    feat(signed-in-only-layout): scaffold - AnnouncementsPage

--- app/(signed-in-only)/settings/announcements/page.tsx
+++ app/(signed-in-only)/settings/announcements/page.tsx
@@ -1,5 +1,16 @@
+import {
+  AppDetailBar,
+  AppDetailBarTitle,
+} from '@/components/ui-unstable/app-detail-bar';
+
 const AnnouncementsPage = () => {
-  return <div>AnnouncementsPage</div>;
+  return (
+    <div>
+      <AppDetailBar>
+        <AppDetailBarTitle>공지사항</AppDetailBarTitle>
+      </AppDetailBar>
+    </div>
+  );
 };
 
 export default AnnouncementsPage;
