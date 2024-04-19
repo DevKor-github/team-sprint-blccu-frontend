a49a8b00453afbe478c4a7f5a89a253075f4d027
Yongjun Park <yopark@student.42seoul.kr>
2024-04-19 16:45:38 +0900

    feat(ui): customize ui component - AlertDialog

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
+import { type VariantProps, cva } from 'class-variance-authority';
 
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -16,47 +17,103 @@ const AlertDialogPortal = AlertDialogPrimitive.Portal;
 const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Overlay
-    className={cn(
-      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-      className,
-    )}
-    {...props}
-    ref={ref}
-  />
-));
-AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName;
+>(({ className, ...props }, ref) => {
+  const context = React.useContext(AlertDialogContentContext);
 
-const AlertDialogContent = React.forwardRef<
-  React.ElementRef<typeof AlertDialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <AlertDialogPortal>
-    <AlertDialogOverlay />
-    <AlertDialogPrimitive.Content
-      ref={ref}
+  if (context === null) {
+    throw new Error(
+      'AlertDialogOverlay must be used inside AlertDialogContent',
+    );
+  }
+
+  return (
+    <AlertDialogPrimitive.Overlay
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        'fixed inset-0 z-50',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+        context.variant === 'default' && 'bg-blccu-black/80',
+        context.variant === 'destructive' && 'bg-blccu-white/80',
         className,
       )}
       {...props}
+      ref={ref}
     />
-  </AlertDialogPortal>
-));
+  );
+});
+AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName;
+
+const alertDialogVariants = cva(
+  /**
+   * @note
+   * - mx-8이 먹히지 않아, 임시로 w-[calc(100vw-32px)]를 사용하였습니다.
+   * - {@link BottomActionSheetContent}에서는 mx-4가 잘 작동한 바 있습니다.
+   */
+  cn(
+    'fixed z-50 bg-blccu-background shadow-lg rounded-lg w-[calc(100vw-32px)]',
+    'duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-left-1/2',
+    'flex flex-col',
+  ),
+  {
+    variants: {
+      side: {
+        center: cn(
+          'left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2',
+          'data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-top-[48%]',
+        ),
+        bottom: cn(
+          'left-1/2 bottom-0 -translate-x-1/2 -translate-y-[32px]',
+          'data-[state=closed]:slide-out-to-top-[30%] data-[state=open]:slide-in-from-top-[30%]',
+        ),
+      },
+      variant: {
+        default: 'bg-blccu-white text-blccu-neutral-800',
+        destructive: 'bg-blccu-neutral-800 text-blccu-neutral-400',
+      },
+    },
+    defaultVariants: {
+      side: 'center',
+      variant: 'default',
+    },
+  },
+);
+
+type AlertDialogContentContextProps = {
+  variant: VariantProps<typeof alertDialogVariants>['variant'];
+};
+
+const AlertDialogContentContext =
+  React.createContext<AlertDialogContentContextProps | null>(null);
+
+interface AlertDialogContentProps
+  extends React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>,
+    VariantProps<typeof alertDialogVariants> {}
+
+const AlertDialogContent = React.forwardRef<
+  React.ElementRef<typeof AlertDialogPrimitive.Content>,
+  AlertDialogContentProps
+>(({ side = 'center', variant = 'default', className, ...props }, ref) => {
+  const contextValue = { variant };
+
+  return (
+    <AlertDialogContentContext.Provider value={contextValue}>
+      <AlertDialogPortal>
+        <AlertDialogOverlay />
+        <AlertDialogPrimitive.Content
+          ref={ref}
+          className={cn(alertDialogVariants({ side, variant }), className)}
+          {...props}
+        />
+      </AlertDialogPortal>
+    </AlertDialogContentContext.Provider>
+  );
+});
 AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName;
 
 const AlertDialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      'flex flex-col space-y-2 text-center sm:text-left',
-      className,
-    )}
-    {...props}
-  />
+  <div className={cn('flex flex-col p-6 text-center', className)} {...props} />
 );
 AlertDialogHeader.displayName = 'AlertDialogHeader';
 
@@ -64,13 +121,7 @@ const AlertDialogFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
-      className,
-    )}
-    {...props}
-  />
+  <div className={cn('flex items-center', className)} {...props} />
 );
 AlertDialogFooter.displayName = 'AlertDialogFooter';
 
@@ -92,7 +143,7 @@ const AlertDialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Description
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn('text-sm', className)}
     {...props}
   />
 ));
@@ -102,29 +153,58 @@ AlertDialogDescription.displayName =
 const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Action
-    ref={ref}
-    className={cn(buttonVariants(), className)}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const context = React.useContext(AlertDialogContentContext);
+
+  if (context === null) {
+    throw new Error(
+      'AlertDialogOverlay must be used inside AlertDialogContent',
+    );
+  }
+
+  return (
+    <AlertDialogPrimitive.Action
+      ref={ref}
+      className={cn(
+        buttonVariants({ variant: 'destructive' }),
+        'flex-1 rounded-none rounded-br-lg border bg-transparent py-4',
+        'focus-visible:ring-inset focus-visible:ring-offset-0',
+        context.variant === 'default' && 'border-blccu-neutral-200',
+        context.variant === 'destructive' && 'border-blccu-neutral-600',
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName;
 
 const AlertDialogCancel = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Cancel>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Cancel>
->(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Cancel
-    ref={ref}
-    className={cn(
-      buttonVariants({ variant: 'secondary' }),
-      'mt-2 sm:mt-0',
-      className,
-    )}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const context = React.useContext(AlertDialogContentContext);
+
+  if (context === null) {
+    throw new Error('AlertDialogCancel must be used inside AlertDialogContent');
+  }
+
+  return (
+    <AlertDialogPrimitive.Cancel
+      ref={ref}
+      className={cn(
+        buttonVariants({ variant: 'secondary' }),
+        'flex-1 rounded-none rounded-bl-lg border border-r-0 bg-transparent py-4',
+        'focus-visible:ring-inset focus-visible:ring-offset-0',
+        context.variant === 'default' && 'border-blccu-neutral-200',
+        context.variant === 'destructive' &&
+          'border-blccu-neutral-600 text-blccu-neutral-200',
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName;
 
 export {
