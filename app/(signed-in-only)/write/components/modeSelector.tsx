e7b1bdddb035a541f608161f9e24ad66d0128992
junelibre <tx0203@gmail.com>
2024-04-26 17:38:33 +0900

    design(ModeSelector): change color to neutral scale

--- app/(signed-in-only)/write/components/modeSelector.tsx
+++ app/(signed-in-only)/write/components/modeSelector.tsx
@@ -14,8 +14,8 @@ const ModeSelector = () => {
       <button
         className={`rounded px-4 py-2 ${
           mode === 'write'
-            ? 'bg-blue-500 text-white'
-            : 'bg-gray-300 text-gray-700'
+            ? 'bg-blccu-neutral-800 text-blccu-white'
+            : 'bg-blccu-neutral-200 text-blccu-neutral-800'
         }`}
         onClick={() => {
           switchMode('write');
@@ -27,8 +27,8 @@ const ModeSelector = () => {
       <button
         className={`rounded px-4 py-2 ${
           mode === 'deco'
-            ? 'bg-blue-500 text-white'
-            : 'bg-gray-300 text-gray-700'
+            ? 'bg-blccu-neutral-800 text-blccu-white'
+            : 'bg-blccu-neutral-200 text-blccu-neutral-800'
         }`}
         onClick={() => {
           switchMode('deco');
