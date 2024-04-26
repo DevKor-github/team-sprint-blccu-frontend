381b5198b8cc59d0ca06e6c2833ed13e1ed9b759
Yongjun Park <yopark@student.42seoul.kr>
2024-04-26 15:48:00 +0900

    design(write-toolbar): extract EditorBottomNavBar
    UI 관련 로직 리팩토링도 함께 진행하였음.

--- app/(signed-in-only)/write/components/toolbar/decoToolbar.tsx
+++ app/(signed-in-only)/write/components/toolbar/decoToolbar.tsx
@@ -1,49 +1,48 @@
 'use-client';
 
-import useFocusedStore from '@/app/(signed-in-only)/write/store/focused';
-import useModeStore from '@/app/(signed-in-only)/write/store/mode';
+import { ArrowDownToLine, File, Smile } from 'lucide-react';
 
-import StickerBottomSheet from './stickerBottomSheet';
+import useFocusedStore from '@/app/(signed-in-only)/write/store/focused';
+import {
+  EditorBottomNavBar,
+  EditorBottomNavBarItem,
+  EditorBottomNavBarItemButton,
+} from '@/components/ui-unstable/editor-bottom-nav-bar';
 
 const DecoToolbar = () => {
   const focused = useFocusedStore((state: any) => state.focused);
-
-  const isFocused = (target: string | null) => {
-    if (focused === target) {
-      return 'text-[#FFFFFF]';
-    }
-    return 'text-[#A6A6A6]';
-  };
-
   const setFocused = useFocusedStore((state: any) => state.setFocused);
 
   return (
-    <div className="fixed bottom-0 left-[50%] flex h-[10%] w-[100vw] max-w-lg -translate-x-[50%] items-center justify-center bg-[#1A1A1A]">
-      <div className="m-auto">
-        {/* <img src='' alt='스티커' /> */}
-        <div
-          className={isFocused('sticker')}
+    <EditorBottomNavBar>
+      <EditorBottomNavBarItem>
+        <EditorBottomNavBarItemButton
+          isSelected={focused === 'text'}
           onClick={() => setFocused('sticker')}
         >
-          스티커
-        </div>
-      </div>
-      <div className="m-auto">
-        {/* <img src='' alt='내지 수정' /> */}
-        <div
-          className={isFocused('background')}
+          <Smile className="h-6 w-6" />
+          <p className="text-xs">스티커</p>
+        </EditorBottomNavBarItemButton>
+      </EditorBottomNavBarItem>
+      <EditorBottomNavBarItem>
+        <EditorBottomNavBarItemButton
+          isSelected={focused === 'background'}
           onClick={() => setFocused('background')}
         >
-          내지 수정
-        </div>
-      </div>
-      <div className="m-auto">
-        {/* <img src='' alt='임시저장' /> */}
-        <div className={isFocused('save')} onClick={() => setFocused('save')}>
-          임시 저장
-        </div>
-      </div>
-    </div>
+          <File className="h-6 w-6" />
+          <p className="text-xs">내지 수정</p>
+        </EditorBottomNavBarItemButton>
+      </EditorBottomNavBarItem>
+      <EditorBottomNavBarItem>
+        <EditorBottomNavBarItemButton
+          isSelected={focused === 'save'}
+          onClick={() => setFocused('save')}
+        >
+          <ArrowDownToLine className="h-6 w-6" />
+          <p className="text-xs">임시 저장</p>
+        </EditorBottomNavBarItemButton>
+      </EditorBottomNavBarItem>
+    </EditorBottomNavBar>
   );
 };
 
---
+++ components/ui-unstable/editor-bottom-nav-bar.tsx
@@ -0,0 +1,52 @@
+import { type PropsWithChildren } from 'react';
+
+import { Button, type ButtonProps } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const EditorBottomNavBar = ({ children }: PropsWithChildren) => {
+  return (
+    <nav
+      className={cn(
+        'fixed bottom-0 left-1/2 w-full max-w-md -translate-x-1/2',
+        'bg-blccu-black text-blccu-white',
+      )}
+    >
+      <ul className="flex h-full items-center justify-evenly">{children}</ul>
+    </nav>
+  );
+};
+
+const EditorBottomNavBarItem = ({ children }: PropsWithChildren) => {
+  return <li>{children}</li>;
+};
+
+type EditorBottomNavBarItemButtonProps = {
+  isSelected?: boolean;
+} & ButtonProps;
+
+/**
+ * @note
+ * 적절한 추상화가 아닐 수 있습니다.
+ */
+const EditorBottomNavBarItemButton = ({
+  isSelected = false,
+  ...props
+}: EditorBottomNavBarItemButtonProps) => {
+  return (
+    <Button
+      variant="ghost"
+      size="none"
+      className={cn(
+        'flex flex-col items-center gap-0.5 px-4 py-3',
+        isSelected ? 'text-blccu-white' : 'text-blccu-neutral-400',
+      )}
+      {...props}
+    />
+  );
+};
+
+export {
+  EditorBottomNavBar,
+  EditorBottomNavBarItem,
+  EditorBottomNavBarItemButton,
+};
