9aa77248468d6fe1125f7c9a84435b135beb2663
Yongjun Park <yopark@student.42seoul.kr>
2024-05-18 17:05:21 +0900

    design(BannerNotSignedIn): change backgroundImage

--- features/root-page/banner-not-signed-in.tsx
+++ features/root-page/banner-not-signed-in.tsx
@@ -3,18 +3,21 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { ROUTES } from '@/constants/routes';
 
+const backgroundImage =
+  'https://images.unsplash.com/photo-1619455061893-0e650d1baab9';
+
 const BannerNotSignedIn = () => {
   return (
     <div
-      className="mx-4 mt-4 flex h-52 flex-col items-center justify-center gap-4 rounded-lg p-4"
+      className="mt-4 flex h-52 flex-col items-center justify-center gap-4 bg-cover bg-center p-4"
       style={{
-        backgroundColor: '#eeeeee',
+        backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      <p className="text-center text-lg font-bold">
+      <p className="text-center text-xl font-bold text-blccu-white">
         블로그도 이제
         <br />
-        <span className="text-2xl">'블꾸'</span> 하세요!
+        <span className="text-3xl">'블꾸'</span> 하세요!
       </p>
       <Link href={ROUTES.SIGN_IN}>
         <Button radius="full">지금 시작하기 </Button>
