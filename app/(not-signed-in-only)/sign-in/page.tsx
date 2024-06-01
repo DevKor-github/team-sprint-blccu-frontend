4b7e91b220ae1a214db227adb7ca056302e81d29
Yongjun Park <yopark@student.42seoul.kr>
2024-06-01 22:53:11 +0900

    feat(sign-in-page): 카카오 로그인 페이지로 변경

--- app/(not-signed-in-only)/sign-in/page.tsx
+++ app/(not-signed-in-only)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import BlccuLogoVertical from '@/assets/svg/blccu-logo-vertical.svg';
 import { Button } from '@/components/ui/button';
-import { ROUTES } from '@/constants/routes';
+import { API_ROUTES, ROUTES } from '@/constants/routes';
 import { KakaoSignInButton } from '@/features/sign-in-page/kakao-sign-in-button';
 
 const SignInPage = () => {
@@ -10,7 +10,7 @@ const SignInPage = () => {
     <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-12">
       <BlccuLogoVertical className="w-16" />
       <div className="flex flex-col items-center gap-5">
-        <Link href={ROUTES.NEW}>
+        <Link href={API_ROUTES.AUTH_KAKAO_LOGIN}>
           <KakaoSignInButton>카카오로 1초만에 로그인하기</KakaoSignInButton>
         </Link>
         <Link href={ROUTES.ROOT}>
