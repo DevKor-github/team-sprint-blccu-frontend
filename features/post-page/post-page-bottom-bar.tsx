82354a302baa8ac9fdce3ac747472792cdea5544
Yongjun Park <yopark@student.42seoul.kr>
2024-06-02 01:01:57 +0900

    feat(post-page-bottom-bar): BE 연동 - 좋아요

--- features/post-page/post-page-bottom-bar.tsx
+++ features/post-page/post-page-bottom-bar.tsx
@@ -1,13 +1,25 @@
 import Link from 'next/link';
 
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Heart, MessageCircleMore, Share2 } from 'lucide-react';
+import { toast } from 'sonner';
 
 import { type PostResponseDto } from '@/__generated__/data-contracts';
 import { CopyCurrentPageTrigger } from '@/components/ui-unstable/copy-current-page-trigger';
-import { Button } from '@/components/ui/button';
 import { IconButton } from '@/components/ui/icon-button';
+import { TOAST_MESSAGES } from '@/constants/messages';
 import { ROUTES } from '@/constants/routes';
+import { api } from '@/lib/api';
 import { cn } from '@/lib/utils';
+import { queries } from '@/queries';
+
+type LikeProps = {
+  postId: number;
+};
+
+type UnlikeProps = {
+  postId: number;
+};
 
 type PostPageBottomBarProps = {
   post: PostResponseDto;
@@ -16,24 +28,69 @@ type PostPageBottomBarProps = {
 const PostPageBottomBar = ({
   post: { id, user, comment_count, like_count },
 }: PostPageBottomBarProps) => {
+  const { data } = useQuery(queries.posts.like(id));
+
+  const queryClient = useQueryClient();
+
+  const { mutate: likeMutate, isPending: isLikePending } = useMutation({
+    mutationFn: ({ postId }: LikeProps) =>
+      api.posts.likesControllerLike(postId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queries.posts.like(id).queryKey,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: queries.posts.detail(id).queryKey,
+      });
+    },
+    onError: () => {
+      toast.error(TOAST_MESSAGES.LIKE_FAIL);
+    },
+  });
+
+  const { mutate: unlikeMutate, isPending: isUnlikePending } = useMutation({
+    mutationFn: ({ postId }: UnlikeProps) =>
+      api.posts.likesControllerDeleteLike(postId),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: queries.posts.like(id).queryKey,
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: queries.posts.detail(id).queryKey,
+      });
+    },
+    onError: () => {
+      toast.error(TOAST_MESSAGES.UNLIKE_FAIL);
+    },
+  });
+
+  const like = data?.data ?? false;
+
   return (
     <div className="fixed bottom-0 mx-auto flex h-14 w-full max-w-md items-center justify-between bg-blccu-white/90 px-2 backdrop-blur-lg">
-      <div className="flex items-center gap-1">
-        <Button
-          size="none"
-          variant="ghost"
-          className="flex items-center gap-1 px-2 py-1"
-        >
-          <Heart
-            className={cn(
-              'h-4 w-4',
-              // like && 'fill-blccu-red text-blccu-red'
-            )}
-          />
+      <div className="flex items-center">
+        <div className="flex items-center gap-1 px-2 py-1">
+          <IconButton
+            size="none"
+            disabled={isLikePending || isUnlikePending}
+            onClick={() => {
+              if (like) {
+                unlikeMutate({ postId: id });
+              } else {
+                likeMutate({ postId: id });
+              }
+            }}
+          >
+            <Heart
+              className={cn('h-4 w-4', like && 'fill-blccu-red text-blccu-red')}
+            />
+          </IconButton>
           <Link href={ROUTES.LIKES_OF(user.handle, id)}>
             <p className="text-sm">{like_count.toLocaleString()}</p>
           </Link>
-        </Button>
+        </div>
         <Link href={ROUTES.COMMENTS_OF(user.handle, id)}>
           <div className="flex items-center gap-1 px-2 py-1">
             <MessageCircleMore className="h-4 w-4" />
