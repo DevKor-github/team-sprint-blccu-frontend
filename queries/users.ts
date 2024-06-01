5f0bf8bb8f36a224f0d14993b7141d7b7156d480
Yongjun Park <yopark@student.42seoul.kr>
2024-06-01 22:44:58 +0900

    feat(queries): posts, users

---
+++ queries/users.ts
@@ -0,0 +1,60 @@
+import { createQueryKeys } from '@lukemorales/query-key-factory';
+
+import { api } from '@/lib/api';
+
+const users = createQueryKeys('users', {
+  me: {
+    queryKey: null,
+    queryFn: api.users.usersControllerFetchUser,
+  },
+  detail: (handle: string) => ({
+    queryKey: [handle],
+    queryFn: () => api.users.usersControllerFindUserByHandle(handle),
+  }),
+  followers: (id: number | undefined) => ({
+    queryKey: [id],
+    queryFn: () => {
+      if (id === undefined) {
+        return Promise.reject(new Error('User ID is undefined'));
+      }
+
+      return api.users.followsControllerGetFollowers(id);
+    },
+  }),
+  following: (id: number | undefined) => ({
+    queryKey: [id],
+    queryFn: () => {
+      if (id === undefined) {
+        return Promise.reject(new Error('User ID is undefined'));
+      }
+
+      return api.users.followsControllerGetFollows(id);
+    },
+  }),
+  categories: (id: number | undefined) => ({
+    queryKey: [id],
+    queryFn: () => {
+      if (id === undefined) {
+        return Promise.reject(new Error('User ID is undefined'));
+      }
+
+      return api.users.postCategoriesControllerFetchPostCategories(id);
+    },
+  }),
+  category: (categoryId: string | undefined) => ({
+    queryKey: [categoryId],
+    queryFn: () => {
+      if (categoryId === undefined) {
+        return Promise.reject(new Error('Category ID is undefined'));
+      }
+
+      return api.users.postCategoriesControllerFetchMyCategory(categoryId);
+    },
+  }),
+  search: (search: string) => ({
+    queryKey: [search],
+    queryFn: () => api.users.usersControllerFindUsersByName(search),
+  }),
+});
+
+export { users };
