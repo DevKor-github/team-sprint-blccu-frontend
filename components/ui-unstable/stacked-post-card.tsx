1bcc6c09a18a8cf1457789448b17338b89450a19
Yongjun Park <yopark@student.42seoul.kr>
2024-06-01 22:49:52 +0900

    feat(ui-unstable): 엔터티에 종속적으로 변경
    - HorizontalScrollablePostCard
    - StackedCategoryCard
    - StackedPostCard
    - StackedUserCard
    - CommentableListItem

--- components/ui-unstable/stacked-post-card.tsx
+++ components/ui-unstable/stacked-post-card.tsx
@@ -6,26 +6,17 @@ import Link from 'next/link';
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 import { format } from 'date-fns';
 
+import { type PostResponseDto } from '@/__generated__/data-contracts';
 import { ROUTES } from '@/constants/routes';
 
 type StackedPostCardProps = {
-  username: string;
-  userHandle: string;
-  title: string;
-  description: string;
-  thumbnail: string;
-  date: Date;
+  post: PostResponseDto;
 };
 
 const StackedPostCard = ({
-  username,
-  userHandle,
-  title,
-  description,
-  thumbnail,
-  date,
+  post: { user, main_image_url, content, date_created, title },
 }: StackedPostCardProps) => {
-  const formattedDate = format(date, 'yyyy. MM. dd.');
+  const formattedDate = format(date_created, 'yyyy. MM. dd.');
 
   return (
     <div className="flex h-20 items-center gap-2 py-2">
@@ -33,12 +24,12 @@ const StackedPostCard = ({
         <h3 className="line-clamp-1 text-sm">{title}</h3>
         <div className="flex flex-col gap-1">
           <p className="line-clamp-2 text-xs font-light text-blccu-neutral-600">
-            {description}
+            {content}
           </p>
           <div className="flex items-center gap-2">
-            <Link href={ROUTES.USER_HANDLE_OF(userHandle)}>
+            <Link href={ROUTES.USER_HANDLE_OF(user.handle)}>
               <p className="line-clamp-1 max-w-28 text-2xs font-light text-blccu-neutral-400">
-                {username}
+                {user.username}
               </p>
             </Link>
             <p className="text-2xs font-light text-blccu-neutral-400">
@@ -50,7 +41,7 @@ const StackedPostCard = ({
       <div className="w-20 flex-shrink-0">
         <AspectRatio ratio={1}>
           <Image
-            src={thumbnail}
+            src={main_image_url}
             alt="thumbnail"
             fill
             sizes="100px"
