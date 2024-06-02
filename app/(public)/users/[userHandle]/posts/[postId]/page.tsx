d4209968835355f0047e91ee095ab5910fe417de
Yongjun Park <yopark@student.42seoul.kr>
2024-06-02 22:27:41 +0900

    feat(post-page): add AuthorProfileSection

--- app/(public)/users/[userHandle]/posts/[postId]/page.tsx
+++ app/(public)/users/[userHandle]/posts/[postId]/page.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { useQuery } from '@tanstack/react-query';
-import { EllipsisVertical, Share2 } from 'lucide-react';
+import { EllipsisVertical } from 'lucide-react';
 
 import {
   AppBar,
   AppBarBack,
   AppBarTitle,
 } from '@/components/ui-unstable/app-bar';
-import { CopyCurrentPageTrigger } from '@/components/ui-unstable/copy-current-page-trigger';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { IconButton } from '@/components/ui/icon-button';
 import { ROUTES } from '@/constants/routes';
 import { PostPageAllPostSection } from '@/features/post-page/post-page-all-post-section';
+import { PostPageAuthorProfileSection } from '@/features/post-page/post-page-author-profile-section';
 import { PostPageBottomBar } from '@/features/post-page/post-page-bottom-bar';
 import { ReportPostBottomActionSheet } from '@/features/post-page/report-post-bottom-action-sheet';
 import { UserHandlePageTrendingPostSection } from '@/features/user-handle-page/user-handle-page-trending-post-section';
@@ -73,21 +73,7 @@ const PostPage = ({ params: { userHandle: _, postId } }: PostPageProps) => {
           width={1280}
           height={2000}
         />
-        <div className="flex items-center">
-          <ReportPostBottomActionSheet
-            id={id}
-            trigger={
-              <IconButton size="lg">
-                <EllipsisVertical className="h-5 w-5" />
-              </IconButton>
-            }
-          />
-          <CopyCurrentPageTrigger asChild>
-            <IconButton size="lg">
-              <Share2 className="h-5 w-5" />
-            </IconButton>
-          </CopyCurrentPageTrigger>
-        </div>
+        <PostPageAuthorProfileSection user={user} />
         <PostPageAllPostSection user={user} />
         <UserHandlePageTrendingPostSection user={user} />
         <div className="h-[24px]" />
--- features/post-page/post-page-author-profile-section.tsx
+++ features/post-page/post-page-author-profile-section.tsx
@@ -1,24 +1,104 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import { type UserPrimaryResponseDto } from '@/__generated__/data-contracts';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import { TOAST_MESSAGES } from '@/constants/messages';
+import { api } from '@/lib/api';
 import { getFollowerDescriptor } from '@/lib/get-descriptor';
-import { type User } from '@/types/mocking-entity';
+import { queries } from '@/queries';
+
+type FollowProps = {
+  userId: number;
+};
+
+type UnfollowProps = {
+  userId: number;
+};
 
 type PostPageAuthorProfileSectionProps = {
-  author: User;
+  user: UserPrimaryResponseDto;
 };
 
 const PostPageAuthorProfileSection = ({
-  author,
+  user: { kakaoId, handle, username, description, profile_image },
 }: PostPageAuthorProfileSectionProps) => {
-  const { username, description, profileImage, followerCount } = author;
+  const { data: userData } = useQuery({
+    ...queries.users.detail(handle),
+    retry: false,
+  });
 
-  const followerDescriptor = getFollowerDescriptor(followerCount);
+  const { data: meData } = useQuery({
+    ...queries.users.me,
+    retry: false,
+  });
+
+  const { data: followerData } = useQuery({
+    ...queries.users.follower(kakaoId),
+    enabled: meData !== undefined,
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutate: followMutate, isPending: isFollowPending } = useMutation({
+    mutationFn: ({ userId }: FollowProps) =>
+      api.users.followsControllerFollowUser(userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queries.users.follower(kakaoId).queryKey,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: queries.users.detail(handle).queryKey,
+      });
+
+      toast.success(TOAST_MESSAGES.FOLLOW_SUCCESS);
+    },
+    onError: () => {
+      toast.error(TOAST_MESSAGES.FOLLOW_FAIL);
+    },
+  });
+
+  const { mutate: unfollowMutate, isPending: isUnfollowPending } = useMutation({
+    mutationFn: ({ userId }: UnfollowProps) =>
+      api.users.followsControllerUnfollowUser(userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queries.users.follower(kakaoId).queryKey,
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: queries.users.detail(handle).queryKey,
+      });
+
+      toast.success(TOAST_MESSAGES.UNFOLLOW_SUCCESS);
+    },
+    onError: () => {
+      toast.error(TOAST_MESSAGES.UNFOLLOW_FAIL);
+    },
+  });
+
+  const user = userData?.data;
+  const me = meData?.data;
+
+  if (user === undefined) {
+    return null;
+  }
+
+  const isSignedIn = me !== undefined;
+
+  const isMe = me?.kakaoId === user.kakaoId;
+
+  const isFollowing = followerData?.data ?? false;
+
+  const followerDescriptor = getFollowerDescriptor(user.follower_count);
 
   return (
-    <section className="mx-4 mt-12 flex items-center rounded-xl py-4 shadow-xl">
+    <section className="mx-4 mt-12 flex items-center rounded-xl py-4 shadow-lg">
       <div className="flex flex-1 flex-col items-center gap-4">
         <Avatar>
-          <AvatarImage src={profileImage} />
+          <AvatarImage src={profile_image} />
           <AvatarFallback className="bg-blccu-neutral-400" />
         </Avatar>
         <div className="flex flex-col items-center gap-1">
@@ -28,8 +108,29 @@ const PostPageAuthorProfileSection = ({
           </p>
         </div>
       </div>
-      <div className="flex flex-1 flex-col items-center gap-1">
-        <Button>팔로우</Button>
+      <div className="flex flex-1 flex-col items-center gap-2">
+        {isSignedIn && !isMe && (
+          <>
+            {isFollowing ? (
+              <Button
+                variant="secondary"
+                radius="full"
+                disabled={isUnfollowPending}
+                onClick={() => unfollowMutate({ userId: user.kakaoId })}
+              >
+                팔로잉
+              </Button>
+            ) : (
+              <Button
+                radius="full"
+                disabled={isFollowPending}
+                onClick={() => followMutate({ userId: user.kakaoId })}
+              >
+                팔로우
+              </Button>
+            )}
+          </>
+        )}
         <p className="text-xs text-blccu-neutral-600">{followerDescriptor}</p>
       </div>
     </section>
