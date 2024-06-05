8ada79992fef14f293727facbbeed4b823fe7789
Yongjun Park <yopark@student.42seoul.kr>
2024-06-06 01:23:47 +0900

    feat(mode-selector): re-design using customized Tabs in shadcn/ui

--- app/(signed-in-only)/write/components/modeSelector.tsx
+++ app/(signed-in-only)/write/components/modeSelector.tsx
@@ -1,43 +1,25 @@
 'use client';
 
-import React, { useState } from 'react';
-
 import useFocusedStore from '@/app/(signed-in-only)/write/store/focused';
 import useModeStore from '@/app/(signed-in-only)/write/store/mode';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const ModeSelector = () => {
-  const mode = useModeStore((state: any) => state.mode);
   const switchMode = useModeStore((state: any) => state.switchMode);
   const setFocused = useFocusedStore((state: any) => state.setFocused);
+
+  const handleValueChange = (value: string) => {
+    switchMode(value);
+    setFocused('init');
+  };
+
   return (
-    <div className="flex items-center justify-center space-x-4">
-      <button
-        className={`rounded px-4 py-2 ${
-          mode === 'write'
-            ? 'bg-blccu-neutral-800 text-blccu-white'
-            : 'bg-blccu-neutral-200 text-blccu-neutral-800'
-        }`}
-        onClick={() => {
-          switchMode('write');
-          setFocused('init');
-        }}
-      >
-        글쓰기
-      </button>
-      <button
-        className={`rounded px-4 py-2 ${
-          mode === 'deco'
-            ? 'bg-blccu-neutral-800 text-blccu-white'
-            : 'bg-blccu-neutral-200 text-blccu-neutral-800'
-        }`}
-        onClick={() => {
-          switchMode('deco');
-          setFocused('init');
-        }}
-      >
-        꾸미기
-      </button>
-    </div>
+    <Tabs defaultValue="write" onValueChange={handleValueChange}>
+      <TabsList className="grid grid-cols-2">
+        <TabsTrigger value="write">글쓰기</TabsTrigger>
+        <TabsTrigger value="deco">꾸미기</TabsTrigger>
+      </TabsList>
+    </Tabs>
   );
 };
 
