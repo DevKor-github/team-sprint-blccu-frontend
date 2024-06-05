d949ec024fb942bab8ec86702e44b4c9b6e3c6e8
Yongjun Park <yopark@student.42seoul.kr>
2024-06-06 01:06:49 +0900

    feat(editor-bottom-sub-nav-bar): 디자인에 맞추어 구현

--- app/(signed-in-only)/write/components/toolbar/subtoolbar/textToolbar.tsx
+++ app/(signed-in-only)/write/components/toolbar/subtoolbar/textToolbar.tsx
@@ -1,6 +1,21 @@
 'use client';
 
+import {
+  Bold,
+  Italic,
+  Ligature,
+  Palette,
+  Strikethrough,
+  Type,
+  Underline,
+} from 'lucide-react';
+
 import useSelectedEditor from '@/app/(signed-in-only)/write/store/selectedEditor';
+import {
+  EditorBottomSubNavBar,
+  EditorBottomSubNavBarItem,
+  EditorBottomSubNavBarItemButton,
+} from '@/components/ui-unstable/editor-bottom-sub-nav-bar';
 
 const TextToolbar = () => {
   const editor = useSelectedEditor((state: any) => state.selectedEditor);
@@ -17,69 +32,70 @@ const TextToolbar = () => {
   };
 
   return (
-    <div className="fixed bottom-[15%] left-[50%] z-50 h-[8%] w-[28rem] -translate-x-[50%] rounded-[20px] bg-[#fffffff5] shadow-[8px_8px_8px_#00000014] backdrop-blur-sm backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(4px)_brightness(100%)]">
-      <div className="flex items-center justify-around py-5 align-middle">
-        <div
-          onClick={() => {
-            if (!editor) return;
-            else editor.chain().focus().setFontFamily('Inter').run();
-          }}
+    <EditorBottomSubNavBar>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor.chain().focus().setFontFamily('Inter').run()}
         >
-          글씨체
-        </div>
-        <div
-          onClick={() => {
-            if (!editor) return;
-            editor.chain().focus().toggleBold().run();
-          }}
+          <Ligature className="h-8 w-8" />
+          <p className="text-xs">글씨체</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor.chain().focus().run()}
         >
-          굵기
-        </div>
-        <div
-          onClick={() => {
-            if (!editor) return;
-            editor.chain().focus().run();
-          }}
+          <Type className="h-8 w-8" />
+          <p className="text-xs">크기</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => document.getElementById('setcolor')?.click()}
         >
-          크기
-        </div>
-        <div>
-          <div onClick={() => document.getElementById('setcolor')?.click()}>
-            색상
-          </div>
+          <Palette className="h-8 w-8" />
+          <p className="text-xs">색상</p>
           <input
             type="color"
             style={{ display: 'none' }}
             onInput={colorInput}
             id="setcolor"
           />
-        </div>
-        <div
-          onClick={() => {
-            if (!editor) return;
-            editor.chain().focus().toggleUnderline().run();
-          }}
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor.chain().focus().toggleBold().run()}
         >
-          밑줄
-        </div>
-        <div
-          onClick={() => {
-            if (!editor) return;
-            editor.chain().focus().toggleStrike().run();
-          }}
+          <Bold className="h-8 w-8" />
+          <p className="text-xs">굵게</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
         >
-          취소선
-        </div>
-        <div
-          onClick={() => {
-            if (!editor) return;
-            editor.chain().focus().toggleItalic().run();
-          }}
+          <Underline className="h-8 w-8" />
+          <p className="text-xs">밑줄</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor.chain().focus().toggleStrike().run()}
         >
-          기울이기
-        </div>
-      </div>
-    </div>
+          <Strikethrough className="h-8 w-8" />
+          <p className="text-xs">취소선</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+      <EditorBottomSubNavBarItem>
+        <EditorBottomSubNavBarItemButton
+          onClick={() => editor?.chain().focus().toggleItalic().run()}
+        >
+          <Italic className="h-8 w-8" />
+          <p className="text-xs">기울이기</p>
+        </EditorBottomSubNavBarItemButton>
+      </EditorBottomSubNavBarItem>
+    </EditorBottomSubNavBar>
   );
 };
 
