a6d2a43ae648dab79c5964250c345579f38001af
Yongjun Park <yopark@student.42seoul.kr>
2024-06-05 19:19:09 +0900

    feat(commenable-list-item): 삭제된 댓글입니다

--- features/comments-page/commentable-list-item.tsx
+++ features/comments-page/commentable-list-item.tsx
@@ -17,39 +17,49 @@ type CommentableListItemProps = {
 };
 
 const CommentableListItem = ({
-  comment: { id, user, content, postsId },
+  comment: { id, user, content, postsId, date_deleted },
   me,
 }: CommentableListItemProps) => {
   const isSignedIn = me !== undefined;
   const isMe = me?.kakaoId === user.kakaoId;
 
+  const isDeleted = date_deleted !== null;
+
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex items-center justify-between">
-        <Link href={ROUTES.USER_HANDLE_OF(user.handle)}>
-          <div className="flex items-center gap-2">
-            <Avatar size="xs">
-              <AvatarImage src={user.profile_image} />
-              <AvatarFallback className="bg-blccu-neutral-400" />
-            </Avatar>
-            <p className="text-sm font-medium">{user.username}</p>
+    <>
+      {isDeleted ? (
+        <p className="text-sm font-medium text-blccu-neutral-600">
+          삭제된 댓글입니다.
+        </p>
+      ) : (
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-between">
+            <Link href={ROUTES.USER_HANDLE_OF(user.handle)}>
+              <div className="flex items-center gap-2">
+                <Avatar size="xs">
+                  <AvatarImage src={user.profile_image} />
+                  <AvatarFallback className="bg-blccu-neutral-400" />
+                </Avatar>
+                <p className="text-sm font-medium">{user.username}</p>
+              </div>
+            </Link>
+            {isSignedIn && (
+              <ReportCommentBottomActionSheet
+                id={id}
+                postId={postsId}
+                isMe={isMe}
+                trigger={
+                  <IconButton>
+                    <EllipsisVertical className="h-4 w-4 text-blccu-neutral-400" />
+                  </IconButton>
+                }
+              />
+            )}
           </div>
-        </Link>
-        {isSignedIn && (
-          <ReportCommentBottomActionSheet
-            id={id}
-            postId={postsId}
-            isMe={isMe}
-            trigger={
-              <IconButton>
-                <EllipsisVertical className="h-4 w-4 text-blccu-neutral-400" />
-              </IconButton>
-            }
-          />
-        )}
-      </div>
-      <p className="text-sm text-blccu-neutral-600">{content}</p>
-    </div>
+          <p className="text-sm text-blccu-neutral-600">{content}</p>
+        </div>
+      )}
+    </>
   );
 };
 
