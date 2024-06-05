9996dd61d0e39f886dfa9b517464b1a8d526823b
junelibre <tx0203@gmail.com>
2024-06-05 20:08:08 +0900

    delete(editor): delete font size feature

--- app/(signed-in-only)/write/components/editor/title.tsx
+++ app/(signed-in-only)/write/components/editor/title.tsx
@@ -1,13 +1,22 @@
 'use client';
 
+import Bold from '@tiptap/extension-bold';
 import Color from '@tiptap/extension-color';
+import Document from '@tiptap/extension-document';
+import DropCursor from '@tiptap/extension-dropcursor';
 import FontFamily from '@tiptap/extension-font-family';
+import GapCursor from '@tiptap/extension-gapcursor';
+import History from '@tiptap/extension-history';
+import Italic from '@tiptap/extension-italic';
+import Paragraph from '@tiptap/extension-paragraph';
 import Placeholder from '@tiptap/extension-placeholder';
+import Strike from '@tiptap/extension-strike';
+import Text from '@tiptap/extension-text';
 import TextAlign from '@tiptap/extension-text-align';
 import TextStyle from '@tiptap/extension-text-style';
 import Underline from '@tiptap/extension-underline';
 import { EditorContent, useEditor } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
+import FontSize from 'tiptap-extension-font-size';
 
 import useEditorContentsStore from '@/app/(signed-in-only)/write/store/editorContents';
 import useModeStore from '@/app/(signed-in-only)/write/store/mode';
@@ -28,7 +37,16 @@ const Title = () => {
 
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      Document,
+      Paragraph,
+      Text,
+      History,
+      DropCursor,
+      GapCursor,
+      Bold,
+      Italic,
+      Strike,
+      FontSize,
       TextStyle,
       Color,
       FontFamily,
