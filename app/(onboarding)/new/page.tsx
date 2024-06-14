77b4725003e3e7ff3a751edd345961c241be20ad
Yongjun Park <yopark@student.42seoul.kr>
2024-06-14 16:49:09 +0900

    chore(new-funnel)!: remove SetProfileStep
    - 플로우 변경 확인 필요

--- app/(onboarding)/new/page.tsx
+++ app/(onboarding)/new/page.tsx
@@ -2,34 +2,36 @@
 
 import { useRouter } from 'next/navigation';
 
-import { SetProfileStep } from '@/app/(onboarding)/new/_steps/set-profile-step';
+import { toast } from 'sonner';
+
 import { SetUsernameStep } from '@/app/(onboarding)/new/_steps/set-username-step';
+import { TOAST_MESSAGES } from '@/constants/messages';
 import { ROUTES } from '@/constants/routes';
 import { useFunnel } from '@/hooks/use-funnel';
 import { getValues } from '@/lib/utils';
 
 const NEW_PAGE_FUNNEL_STEPS = {
   SET_USERNAME: 'set-username',
-  SET_PROFILE: 'set-profile',
 } as const;
 
 const NewPage = () => {
-  const { Funnel, setStep } = useFunnel({
+  const { Funnel } = useFunnel({
     steps: getValues(NEW_PAGE_FUNNEL_STEPS),
     initialStep: NEW_PAGE_FUNNEL_STEPS.SET_USERNAME,
   });
 
   const router = useRouter();
 
+  const handleStepComplete = () => {
+    router.push(ROUTES.ROOT);
+
+    toast.success(TOAST_MESSAGES.WELCOME);
+  };
+
   return (
     <Funnel>
       <Funnel.Step name={NEW_PAGE_FUNNEL_STEPS.SET_USERNAME}>
-        <SetUsernameStep
-          onNext={() => setStep(NEW_PAGE_FUNNEL_STEPS.SET_PROFILE)}
-        />
-      </Funnel.Step>
-      <Funnel.Step name={NEW_PAGE_FUNNEL_STEPS.SET_PROFILE}>
-        <SetProfileStep onNext={() => router.push(ROUTES.ROOT)} />
+        <SetUsernameStep onNext={handleStepComplete} />
       </Funnel.Step>
     </Funnel>
   );
