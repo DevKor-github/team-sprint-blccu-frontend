668e5a602178f15de428eee3bf0e225754479423
Yongjun Park <yopark@student.42seoul.kr>
2024-06-14 22:17:35 +0900

    refactor(dialog): create options - showClose, fullScreen

--- app/(signed-in-only)/write/dialog/publish-post-dialog.tsx
+++ app/(signed-in-only)/write/dialog/publish-post-dialog.tsx
@@ -6,7 +6,7 @@ const PublishPostDialog = ({ trigger }: PropsWithTrigger) => {
   return (
     <Dialog>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent className="h-full w-full max-w-screen-sm p-0">
+      <DialogContent fullScreen className="h-full w-full max-w-screen-sm p-0">
         <PublishPostForm />
       </DialogContent>
     </Dialog>
