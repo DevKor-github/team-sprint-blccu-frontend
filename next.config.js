47fd031de59f9689c22f50add215f9e17012a83c
Yongjun Park <yopark@student.42seoul.kr>
2024-07-16 20:46:57 +0900

    fix(pwa): disable not in prod env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: !isProd,
 });
 
 /** @type {import('next').NextConfig} */
