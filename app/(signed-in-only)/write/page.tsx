5495df84447e1d3bc4a5f0fdc89b18db0c69f13e
junelibre <tx0203@gmail.com>
2024-09-30 15:22:20 +0900

    typo

--- app/(signed-in-only)/write/page.tsx
+++ app/(signed-in-only)/write/page.tsx
@@ -3,9 +3,8 @@ import { useEffect } from 'react';
 import { MainToolbar } from '@/app/(signed-in-only)/write/_components/toolbar/main-toolbar';
 import { SubToolbar } from '@/app/(signed-in-only)/write/_components/toolbar/sub-toolbar';
 import { WritePageContent } from '@/app/(signed-in-only)/write/_components/write-page-content';
+import { useEditorContentsStore } from '@/app/(signed-in-only)/write/_store/use-editor-contents-store';
 import { useStickersStore } from '@/app/(signed-in-only)/write/_store/use-stickers-store';
-
-import { useEditorContentsStore } from './_store/use-editor-contents-store';
 
 const WritePage = () => {
   const { setStickers } = useStickersStore();
